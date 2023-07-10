9158f5b99cfc9428d81fd4e674c23404b75d6e40
Anastasia Moskalyova <amsklva@yandex.ru>
2023-07-10 16:20:22 +0300

    feat: Сформирован класс Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,14 +1,60 @@
 export class Popup {
-    constructor(popupSelector){
-        this._popupSelector = popupSelector;
+  constructor(popupSelector) {
+    // this._popupSelector = popupSelector;
+    this._popup = document.querySelector(popupSelector);
+  }
 
+  open() {
+    this.setEventListeners();
+    this._popup.classList.add("popup_opened");
+  }
+
+  close() {
+    this._removeEventListeners();
+    this._popup.classList.remove("popup_opened");
+  }
+
+  _openPopup() {
+    this.open();
+  }
+
+  _closePopup(evt) {
+    if (evt.target.classList.contains("popup_opened")){
+        this.close();
     }
+  }
 
-    open () {
 
+  _closePopupOnEsc(evt) {
+    if (evt.key === "Escape") {
+      const activePopup = document.querySelector(".popup_opened");
+      this._closePopup(activePopup);
     }
+  }
 
-    close () {
-
+  _closePopupOnOverlay(evt) {
+    if (evt.target.classList.contains("popup_opened")) {
+        this.close();
     }
+  }
+
+  // функция закрытия попапа
+//   setEventListeners() {
+//     popupCloseButtons.forEach((button) => {
+//       const buttonsPopup = button.closest(".popup");
+//       button.addEventListener("click", () => closePopup(buttonsPopup));
+//       buttonsPopup.addEventListener("click", closePopupOnOverlay);
+//     });
+//   }
+  setEventListeners() {
+    this._popup.addEventListener('mousedown', this._closePopup);
+    this._popup.addEventListener('mousedown', this._closePopupOnOverlay);
+    document.addEventListener('keydown', this._closePopupOnEsc);
+  }
+
+  _removeEventListeners() {
+    this._popup.removeEventListener('mousedown', this._closePopup);
+    this._popup.removeEventListener('mousedown', this._closePopupOnOverlay);
+    document.removeEventListener('keydown', this._closePopupOnEsc);
+  }
 }
